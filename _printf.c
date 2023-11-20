85a70b4b6034b6eff1e9988f2b4e71bb61db5777
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-20 17:54:54 +0100

    All the needed function on the same files to test. Various tests to increment _putchar

--- _printf.c
+++ _printf.c
@@ -1,6 +1,20 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
+
+typedef struct func_selec
+{
+	char *specifier;
+	int (*func_call)(va_list);
+} call;
+
+int _printchar(va_list c)
+{
+	int c;
+
+	_putchar(va_arg(c, int));
+}
 
 /**
  * _printf - writes the output to stdout
@@ -14,26 +28,37 @@ int _printf(const char *format, ...)
 	int i;
 	int len = strlen(format);
 
-	if (format == NULL)
-		return (-1);
+	va_list ap;
+
+	va_start(ap, format);
+
+	call func_select[] = {
+		{"c", _printchar},
+		/*{"s", _printstring},*/
+		/*{"%", _put_percent},*/
+		{NULL, NULL}
+	};
 
 	for (i = 0; i < len; i++)
 	{
 
-		if (format[i] == '%' && format[i + 1] == 's')
+		if (format[i] == '%')
 		{
-			printf("Appel fonction string");
-			_printstring(format[i]);
-			i += 2;
+			if (format[i + 1] == *func_select[0].specifier)
+				func_select[0].func_call(ap);
 		}
 
-		if (format[i] == '%' && format[i + 1] == '%')
+		/*if (format[i] == '%' && format[i + 1] == '%')
 		{
 			putchar('%');
 			i += 2;
 		}
+		*/
+
+	va_end(ap);
 
 	putchar(format[i]);
+
 	}
 
 	return (len);
--- main.h
+++ main.h
@@ -1,9 +1,16 @@
 #ifndef __protect__
 #define __protect__
 
+
+typedef struct func_selec
+{
+	const char *specifier;
+	int (*func_call)(char);
+} call;
+
 int _printf(const char *format, ...);
-int _putchar(char c);
-/*Fonction pour integer*/
+int _printchar(char c);
 void (_printstring(char *s));
+int _put_percent(char c);
 
 #endif
