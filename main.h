4b85e9db63aa769167a1721c134cad1b491fad3c
Florent1711 <graphit01@hotmail.com>
2023-11-21 06:13:54 -0800

    Added main.h - proper version

--- main.h
+++ main.h
@@ -1,16 +1,24 @@
-#ifndef __protect__
-#define __protect__
+#define __holberton_h__
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
-
-typedef struct func_selec
+/**
+ * struct call - selects the right function to associate with %
+ * @specifier: which function to call
+ * @func_call: name of the array
+ */
+typedef struct call
 {
-	const char *specifier;
-	int (*func_call)(char);
+	char *specifier;
+	int (*func_call)(va_list);
 } call;
 
 int _printf(const char *format, ...);
-int _printchar(char c);
-void (_printstring(char *s));
+int _printchar(va_list c);
+int _putchar(char c);
+int (_printstring(char *s));
 int _put_percent(char c);
 
 #endif
