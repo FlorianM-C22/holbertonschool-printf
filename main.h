e78fb34f0cc74bc1d5d56f8a1a2ae5a808eb847e
FlorianM-C22 <7484@holbertonstudents.com>
2023-11-22 12:54:15 +0100

    Clean main.h

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef __PROTECT__
+#define __PROTECT__
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * struct call - selects the right function to associate with %
+ * @specifier: which function to call
+ * @func_call: name of the array
+ */
+typedef struct call
+{
+	char *specifier;
+	int (*func_call)(va_list);
+} call;
+
+int _printf(const char *format, ...);
+int _printchar(va_list ap);
+int (_printstring(va_list ap));
+int _put_percent(va_list ap);
+int func_select(const char *format, struct call func_array[], va_list ap);
+
+#endif
